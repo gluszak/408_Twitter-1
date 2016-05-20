885975d5446c66fff477a571f60f16f32aa1c904
Łukasz Pokrzywa <lukasz.pokrzywa@gmail.com>
2016-05-20 12:09:16 +0200

    added task 1

---
+++ src/connection.php
@@ -0,0 +1,17 @@
+<?php
+
+$servername = "localhost";
+$username = "root";
+$password = "coderslab";
+$baseName = "Twitter";
+
+//Tworzenie nowego połączenia:
+$conn = new mysqli($servername, $username, $password, $baseName);
+
+//Sprawdzamy, czy połączenie się udało:
+if($conn->connect_error) {
+	die("Połączenie nieudane. Błąd: ".$conn->connect_error);
+} else {
+	$conn->set_charset("utf8");
+	echo "Połączenie udane<br />";
+}
